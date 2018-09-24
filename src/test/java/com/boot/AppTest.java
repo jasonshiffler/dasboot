9771402530799b3b68bc490a26dc6674a9c875e0
Jason Shiffler <jasonshiffler@yahoo.com>
2018-09-23 21:32:54 -0500

    UnitTest Added

--- src/test/java/com/boot/AppTest.java
+++ src/test/java/com/boot/AppTest.java
@@ -1,11 +1,20 @@
 package com.boot;
 
 
+import com.boot.controller.HomeController;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    @Test
+    public void testApp() {
+        HomeController hc = new HomeController();
+        String result = hc.home();
+        assertEquals(result, "Das Booty, reporting for Duty!");
 
+    }
 }
